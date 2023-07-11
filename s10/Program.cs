6a34b0af90de91a2cbad19124084b9d58a6c93c1
Ekaterina <greenpantera@rambler.ru>
2023-07-11 23:13:08 +0500

    Добавлена формулировка для задачи 10

--- s10/Program.cs
+++ s10/Program.cs
@@ -1,4 +1,6 @@
-﻿int num = new Random().Next(100,1000);
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
 int digit1 = num/10;
 int digit2 = digit1%10;
 Console.WriteLine($"Вторая цифра числа {num} будет {digit2}");
